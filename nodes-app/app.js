cba1f6284f9ec82f1edaf25f44b17b613b520770
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-09-16 16:44:37 +0100

    Importing Node.js Core Modules

---
+++ nodes-app/app.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'My name is precious')
+
+
+// Challenge: Append a message to noted.txt
+
+fs.appendFileSync('notes.txt', "\nI'm about to be a Certified Fullstack Dev!")
